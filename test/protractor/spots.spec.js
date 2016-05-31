0c05856c33191d7fb9a42eb1b9d105688043ec5b
Thorsten Binias <thorsten@binias-online.de>
2016-05-31 21:13:28 +0200

    add more tests for spots view

--- test/protractor/spots.spec.js
+++ test/protractor/spots.spec.js
@@ -15,6 +15,23 @@ describe('spots', function() {
             expect(element.all(by.css('.spotmate-ranking .rank')).count()).toBe(9);
         });
 
+        it('should render ranks for Fri to Sun on first spot', function() {
+            expect(element.all(by.css('.spotmate-ranking .rank')).get(0).element(by.binding('key')).getText())
+                .toBe('Fri 27.05');
+            expect(element.all(by.css('.spotmate-ranking .rank')).get(1).element(by.binding('key')).getText())
+                .toBe('Sat 28.05');
+            expect(element.all(by.css('.spotmate-ranking .rank')).get(2).element(by.binding('key')).getText())
+                .toBe('Sun 29.05');
+        });
+
+        it('should render 8h to 17h for each rank', function() {
+            element.all(by.css('.spotmate-ranking .rank')).each(function(element, index) {
+                expect(element.getText()).toContain('8h');
+                expect(element.getText()).toContain('11h');
+                expect(element.getText()).toContain('14h');
+                expect(element.getText()).toContain('17h');
+            });
+        });
     });
 
 });
